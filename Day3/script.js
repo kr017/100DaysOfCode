4c21aebe2ba8d72468ad5ffad9a9f89cfd1ccdf7
Kiran <krmate634@gmail.com>
2021-01-02 00:48:01 +0530

    action for click of uparrow

---
+++ Day3/script.js
@@ -0,0 +1,10 @@
+const btn=document.getElementById("uparrow");
+
+btn.addEventListener("click",()=>{
+
+// alert("hellow")
+window.scroll({top: 0, left: 0, behavior: 'smooth'});
+
+})
+
+btn.addEventListener
